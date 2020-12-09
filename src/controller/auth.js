0fae89db18512ba7c26d0034f7a1c51db56478ac
Rizwan Khan <rizwankhan.august16@gmail.com>
2020-12-09 10:11:48 -0800

    auth user signup error handling

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -13,7 +13,7 @@ exports.signup = (req, res) => {
   User.findOne({ email: req.body.email }).exec(async (error, user) => {
     if (user)
       return res.status(400).json({
-        message: "User already registered",
+        error: "User already registered",
       });
 
     const { firstName, lastName, email, password } = req.body;
